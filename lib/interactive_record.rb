308d59e44a6f4e71f26297064dd6bb7a351032bb
Tyler Hunt <tyhunt04@gmail.com>
2019-03-22 17:16:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -27,7 +27,7 @@ class InteractiveRecord
   end
 
   def table_name_for_insert
-    self.class.table_name_for_insert
+    self.class.table_name
   end
 
   def col_names_for_insert
@@ -47,5 +47,6 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0]
   end
+
 
 end
