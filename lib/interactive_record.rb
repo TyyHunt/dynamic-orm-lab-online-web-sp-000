9234339889b577d69c6e51aecfb76ecd8ac86fba
Tyler Hunt <tyhunt04@gmail.com>
2019-03-22 17:17:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -31,7 +31,7 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if? {|value| value == "id"}.join(", ")
+    self.class.column_names.delete_if {|value| value == "id"}.join(", ")
   end
 
   def values_for_insert
