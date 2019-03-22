a33600e8070459ae988a95c3e14e54feaf19f549
Tyler Hunt <tyhunt04@gmail.com>
2019-03-22 17:16:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,26 +8,5 @@ class Student < InteractiveRecord
     attr_accessor col_name.to_sym
   end
 
-  def table_name_for_insert
-    self.class.table_name
-  end
-
-  def col_names_for_insert
-    self.class.col_names.delete_if? {|value| value == "id"}.join(", ")
-  end
-
-  def values_for_insert
-    values = []
-    self.class.col_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
-  end
-
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0]
-  end
 
 end
