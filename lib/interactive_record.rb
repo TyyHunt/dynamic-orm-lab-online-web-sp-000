432eecc08ba0e2a0ad1df6622d3efac262f61fe3
Tyler Hunt <tyhunt04@gmail.com>
2019-03-22 17:47:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,7 +17,7 @@ class InteractiveRecord
     table_info.each do |column|
      column_names << column["name"]
     end
-    column_names
+    column_names.compact
   end
 
   def initialize(options={})
